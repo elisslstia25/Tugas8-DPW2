2b5f508cd64fde3914d59a44aa88bc5da35248a5
Elis Sulistia Sudrajat <elisslstia25@gmail.com>
2021-11-25 11:43:41 +0700

    Tugas 9

---
+++ project9_dpw2/system/resources/views/admin/section/sidebar.blade.php
@@ -0,0 +1,42 @@
+ <div class="sidebar-wrapper">
+        <div class="logo">
+          <a href="javascript:void(0)" class="simple-text logo-mini">
+            FH
+          </a>
+          <a href="javascript:void(0)" class="simple-text logo-normal">
+            Awesome Team
+          </a>
+        </div>
+        <ul class="nav">
+          <li class="active ">
+            <a href="{{url('admin')}}/beranda">
+              <i class="tim-icons icon-chart-pie-36"></i>
+              <p>Beranda</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{url('admin')}}/produk">
+              <i class="tim-icons icon-atom"></i>
+              <p>Produk</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{url('admin')}}/kategori">
+              <i class="tim-icons icon-bell-55"></i>
+              <p>Kategori</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{url('admin')}}/user">
+              <i class="tim-icons icon-single-02"></i>
+              <p>User</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{url('login')}}">
+              <i class="tim-icons icon-single-02"></i>
+              <p>Log Out</p>
+            </a>
+          </li>
+        </ul>
+      </div>
